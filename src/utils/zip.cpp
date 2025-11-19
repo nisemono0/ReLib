17c6946c4a40f89667c5e6a4a3370acbbe7565ec
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2025-11-19 19:19:53 +0200

    Fix a bug where the json file search would not be found if not inside the root of the archive file

--- src/utils/zip.cpp
+++ src/utils/zip.cpp
@@ -2,6 +2,8 @@
 #include "utils/fs.hpp"
 #include "utils/defs.hpp"
 
+#include "base/log.hpp"
+
 #include <quazip/JlCompress.h>
 #include <quazip/quazipfile.h>
 
@@ -13,30 +15,57 @@
 #include <QCryptographicHash>
 #include <QJsonDocument>
 #include <QVariantMap>
+#include <QCollator>
 
 
 QJsonDocument Utils::Zip::getInfoJsonDocument(const QString &file_path) {
-    QuaZipFile zip_file = QuaZipFile(file_path);
-    zip_file.setFileName(ZipInfo::InfoJson, QuaZip::csInsensitive);
+    QuaZip zip_archive = QuaZip(file_path);
 
-    // Will opne only if filename set with setFileName exists
-    if (zip_file.open(QIODevice::ReadOnly)) {
-        QByteArray json_byte_array = zip_file.readAll();
-        zip_file.close();
-
-        QJsonDocument json_doc = QJsonDocument::fromJson(json_byte_array);
-        // If inside json.info is empty or it's not a json file return an empty QJsonObject
-        if (json_doc.isNull() || !json_doc.isObject()) {
-            return QJsonDocument();
-        }
-
-        return json_doc;
+    // If zip archive can't be openend return empty json
+    if (!zip_archive.open(QuaZip::mdUnzip)) {
+        zip_archive.close();
+        Log::error(QStringLiteral("[Utils::Zip::getInfoJsonDocument:zip_archive.open]: %1").arg(file_path));
+        return QJsonDocument();
     }
 
-    if (zip_file.isOpen()) {
-        zip_file.close();
+    // Get ZipInfo::InfoJson file from archive
+    QStringList info_json_file = zip_archive.getFileNameList().filter(ZipInfo::InfoJson, Qt::CaseInsensitive);
+    // If no file is found, close the archive and return an empty json
+    if (info_json_file.isEmpty()) {
+        zip_archive.close();
+        return QJsonDocument();
     }
-    // Return emtpy QJsonDocument if info.json doesn't exist
-    return QJsonDocument();
+    // Try and set current file inside archive to our json file
+    // Return empty json on error
+    if (!zip_archive.setCurrentFile(info_json_file[0])) {
+        zip_archive.close();
+        return QJsonDocument();
+    }
+
+    // json file inside our archive
+    QuaZipFile zip_file = QuaZipFile(&zip_archive);
+    // Try to open json file inside our archive for reading
+    // Return empty json on error
+    if (!zip_file.open(QIODevice::ReadOnly)) {
+        zip_file.close();
+        zip_archive.close();
+        Log::error(QStringLiteral("[Utils::Zip::getInfoJsonDocument:zip_file.open]: %1").arg(file_path));
+        return QJsonDocument();
+    }
+
+    // Read json QByteArray and close zip_file and zip_archive
+    QByteArray json_byte_array = zip_file.readAll();
+    zip_file.close();
+    zip_archive.close();
+
+    QJsonDocument json_doc = QJsonDocument::fromJson(json_byte_array);
+
+    // If the json inside the archive is empty or if it's not a json file
+    // return and empty QJsonDocument
+    if (json_doc.isNull() || !json_doc.isObject()) {
+        return QJsonDocument();
+    }
+
+    return json_doc;
 }
 
