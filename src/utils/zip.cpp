70fa15044eb80a6b37f7b22fd20c72878e36b96b
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2025-11-19 20:09:03 +0200

    Add util function to get the first image from an archive as QByteArray * Also cleaned includes

--- src/utils/zip.cpp
+++ src/utils/zip.cpp
@@ -1,5 +1,5 @@
 #include "utils/zip.hpp"
-#include "utils/fs.hpp"
+#include "utils/str.hpp"
 #include "utils/defs.hpp"
 
 #include "base/log.hpp"
@@ -8,14 +8,10 @@
 #include <quazip/quazipfile.h>
 
 #include <QStringList>
-#include <QFile>
-#include <QFileInfo>
-#include <QDir>
-#include <QDirIterator>
-#include <QCryptographicHash>
 #include <QJsonDocument>
-#include <QVariantMap>
 #include <QCollator>
+#include <QMimeDatabase>
+#include <QMimeType>
 
 
 QJsonDocument Utils::Zip::getInfoJsonDocument(const QString &file_path) {
@@ -39,23 +35,24 @@ QJsonDocument Utils::Zip::getInfoJsonDocument(const QString &file_path) {
     // Return empty json on error
     if (!zip_archive.setCurrentFile(info_json_file[0])) {
         zip_archive.close();
+        Log::error(QStringLiteral("[Utils::Zip::getInfoJsonDocument:zip_json.setCurrentFile]: %1").arg(file_path));
         return QJsonDocument();
     }
 
     // json file inside our archive
-    QuaZipFile zip_file = QuaZipFile(&zip_archive);
+    QuaZipFile zip_json = QuaZipFile(&zip_archive);
     // Try to open json file inside our archive for reading
     // Return empty json on error
-    if (!zip_file.open(QIODevice::ReadOnly)) {
-        zip_file.close();
+    if (!zip_json.open(QIODevice::ReadOnly)) {
+        zip_json.close();
         zip_archive.close();
-        Log::error(QStringLiteral("[Utils::Zip::getInfoJsonDocument:zip_file.open]: %1").arg(file_path));
+        Log::error(QStringLiteral("[Utils::Zip::getInfoJsonDocument:zip_json.open]: %1").arg(file_path));
         return QJsonDocument();
     }
 
-    // Read json QByteArray and close zip_file and zip_archive
-    QByteArray json_byte_array = zip_file.readAll();
-    zip_file.close();
+    // Read json QByteArray and close zip_json and zip_archive
+    QByteArray json_byte_array = zip_json.readAll();
+    zip_json.close();
     zip_archive.close();
 
     QJsonDocument json_doc = QJsonDocument::fromJson(json_byte_array);
@@ -67,5 +64,59 @@ QJsonDocument Utils::Zip::getInfoJsonDocument(const QString &file_path) {
     }
 
     return json_doc;
+}
+
+QByteArray Utils::Zip::getArchiveFirstImage(const QString &file_path) {
+    QuaZip zip_archive = QuaZip(file_path);
+
+    if (!zip_archive.open(QuaZip::mdUnzip)) {
+        zip_archive.close();
+        Log::error(QStringLiteral("[Utils::Zip::getArchiveFirstImage:zip_archive.open]: %1").arg(file_path));
+        return QByteArray();
+    }
+
+    // Get archive files list and natural sort it
+    QStringList archive_files_list = zip_archive.getFileNameList();
+    Utils::Str::naturalSortQStringList(archive_files_list);
+
+    // Get the first found image file name inside the archive
+    QString image_file = QString();
+    QMimeDatabase mime_database = QMimeDatabase();
+    for (auto file_name : archive_files_list) {
+        QMimeType file_mime = mime_database.mimeTypeForFile(file_name);
+        if (file_mime.isValid() && file_mime.name().startsWith("image/", Qt::CaseInsensitive)) {
+            image_file = file_name;
+            break;
+        }
+    }
+
+    // If no image file is found return empty QByteArray
+    if(Utils::Str::isNullOrEmpty(image_file)) {
+        zip_archive.close();
+        return QByteArray();
+    }
+
+    // Try and set the current file to our found image file
+    if (!zip_archive.setCurrentFile(image_file)) {
+        zip_archive.close();
+        Log::error(QStringLiteral("[Utils::Zip::getArchiveFirstImage:zip_archive.open]: %1").arg(file_path));
+        return QByteArray();
+    }
+
+    QuaZipFile zip_image = QuaZipFile(&zip_archive);
+    // Try to open our image file for reading
+    // Return empty QByteArray on error
+    if (!zip_image.open(QIODevice::ReadOnly)) {
+        zip_image.close();
+        zip_archive.close();
+        Log::error(QStringLiteral("[Utils::Zip::getArchiveFirstImage:zip_image.open]: %1").arg(file_path));
+        return QByteArray();
+    }
+
+    QByteArray image_bytes = zip_image.readAll();
+    zip_image.close();
+    zip_archive.close();
+
+    return image_bytes;
 }
 
--- src/utils/zip.hpp
+++ src/utils/zip.hpp
@@ -2,11 +2,13 @@
 
 #include <QString>
 #include <QJsonObject>
-#include <QVariantMap>
+#include <QByteArray>
 
 
 namespace Utils::Zip {
     // Return a QJsonDocument with the archive's json.info or an empty one if none are found
     QJsonDocument getInfoJsonDocument(const QString &file_path);
+    // Return a QByteArray of the first image in the archive
+    QByteArray getArchiveFirstImage(const QString &file_path);
 };
 
